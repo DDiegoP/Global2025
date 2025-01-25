e1995f6541760ae6cd6e761bcd34e9eb0e08cd58
Miguel Curros García <micurros@ucm.es>
2025-01-25 17:57:36 +0100

    Input loco

--- Global2025/Assets/Scripts/Input/SensorsInput.cs
+++ Global2025/Assets/Scripts/Input/SensorsInput.cs
@@ -6,58 +6,81 @@ using Gyroscope = UnityEngine.InputSystem.Gyroscope;
 public class SensorsInput : MonoBehaviour
 {
     [SerializeField]
-    float scale_x = 5f, scale_y = 7f, gyro_dz = 0.15f, accel_dz = 0.5f;
+    RectTransform canvas;
+    [SerializeField]
+    float max_x_angle = 45f, max_y_angle = 30f;
 
-    InputData lastInput;
+    Vector3 attitude_reference;
+
+    const float no_gravity = 9.8f;
+
+    InputData inputData;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        lastInput = new InputData();
-        lastInput.mouse_pos = Vector2.zero;
-        lastInput.mouse_rotation = 0f;
-        lastInput.clicked = false;
+        inputData = new InputData();
+        inputData.mouse_pos = Vector2.zero;
+        inputData.mouse_rotation = 0f;
+        inputData.clicked = false;
 
-        InputSystem.EnableDevice(Gyroscope.current);
-        InputSystem.EnableDevice(Accelerometer.current);
+        InputSystem.EnableDevice(AttitudeSensor.current);
+        InputSystem.EnableDevice(StepCounter.current);
+
+        attitude_reference = AttitudeSensor.current.attitude.value.eulerAngles;
     }
 
     private void OnDestroy() {
-        InputSystem.DisableDevice(Accelerometer.current);
-        InputSystem.DisableDevice(Gyroscope.current);
+        InputSystem.DisableDevice(StepCounter.current);
+        InputSystem.DisableDevice(AttitudeSensor.current);
     }
 
-    Vector2 GetMousePositionFromGyro(Vector3 gyro) {
-        return lastInput.mouse_pos + new Vector2(-scale_x * (gyro.z) * Time.deltaTime, scale_y * (gyro.x) * Time.deltaTime);
+    public void SetAsReference() {
+        attitude_reference = AttitudeSensor.current.attitude.value.eulerAngles;
     }
 
-    InputData GetInput() {
-        InputData input = new InputData();
-        
-        if (!Gyroscope.current.enabled)
-            return input;
+    Vector2 GetMousePositionFromSensors(Vector3 atti) {
+        return new Vector2(-NormalizeAttitude(atti.z, attitude_reference.z, max_x_angle), NormalizeAttitude(atti.x, attitude_reference.x, max_y_angle));
+    }
 
-        // Z -> X && X -> Y
-        Vector3 gyro = Gyroscope.current.angularVelocity.value;
+    float floatModule(float a, float b) {
+        return a - Mathf.Floor(a / b) * b;
+    }
 
-        input.mouse_pos = GetMousePositionFromGyro(gyro);
-        input.mouse_rotation = -gyro.y;
-        input.clicked = false;
-        
-        return input;
+    float NormalizeAttitude(float atti, float atti_reference, float max_angle) {
+        return Mathf.Clamp((floatModule(atti - atti_reference + 180f, 360f) - 180f) / max_angle, -1f, 1f);
+    }
+    
+    float GetMouseRotationFromSensors(Vector3 atti) {
+        return atti.z - attitude_reference.z;
+    }
+
+    void ClickButton() {
+        inputData.clicked = true;
+    }
+
+    void GetInput() {
+        Vector3 atti = AttitudeSensor.current.attitude.value.eulerAngles;
+        Debug.Log(StepCounter.current.stepCounter.value);
+        inputData.mouse_pos = GetMousePositionFromSensors(atti);
+        inputData.mouse_rotation = GetMouseRotationFromSensors(atti);
     }
 
     void SendInputData(InputData input) {
         // TO DO
 
         // VV Temp VV
-        transform.position = new Vector3(input.mouse_pos.x, input.mouse_pos.y, transform.position.z);
-        transform.Rotate(0, 0, input.mouse_rotation);
+        RectTransform trans = GetComponent<RectTransform>();
+        trans.anchoredPosition = new Vector3((canvas.sizeDelta.x / 2f) * input.mouse_pos.x, (canvas.sizeDelta.y / 2f) * input.mouse_pos.y, transform.position.z);
+        trans.rotation = Quaternion.Euler(0, 0, input.mouse_rotation);
     }
 
     // Update is called once per frame
     void Update() {
-        InputData currentInput = GetInput();
-        SendInputData(currentInput);
-        lastInput = currentInput;
+        GetInput();
+    }
+
+    private void LateUpdate() {
+        SendInputData(inputData);
+        inputData.clicked = false;
     }
 }
