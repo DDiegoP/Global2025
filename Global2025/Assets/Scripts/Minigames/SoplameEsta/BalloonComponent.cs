ca29904ba10096dabbe594d9030e92a28b76d887
Rafael Argandoña Blacido <rargan01@ucm.es>
2025-01-25 12:57:21 +0100

    Prefabs y botella

--- Global2025/Assets/Scripts/Minigames/SoplameEsta/BalloonComponent.cs
+++ Global2025/Assets/Scripts/Minigames/SoplameEsta/BalloonComponent.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
 
-public class BalloonComponent : MonoBehaviour
+public class BalloonComponent : GameComponent
 {
+    private double _air = 0.0;
+
+    private double _scale = 1.0;
+
+    private bool _blowing = false;
+
+    [SerializeField]
+    private float maxScale = 4;
+
+    private void Pop()
+    {
+
+    }
+
+    private void ApplyScore()
+    {
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +30,17 @@ public class BalloonComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Se pilla el input de soplido y se aumenta el "aire" del globo
+        // if (!blowing) VER SI EMPIEZA A SOPLAR
+
+        if (_blowing)
+        {
+            // Aumentamos el aire del globo (y con ello la escala)
+            _air += 0.05;
+            _scale += _air;
+            if (_air)
+            // Si deja de soplar ->
+            
+        }
     }
 }
