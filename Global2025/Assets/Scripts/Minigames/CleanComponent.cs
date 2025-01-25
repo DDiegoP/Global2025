221074fb539e63bd02d9b01aa97ef316477977d8
Andrea Vega Saugar <anvega03@ucm.es>
2025-01-25 17:34:04 +0100

    nueva mano, assets y nueva escena

--- Global2025/Assets/Scripts/Minigames/CleanComponent.cs
+++ Global2025/Assets/Scripts/Minigames/CleanComponent.cs
@@ -39,23 +39,53 @@ public class CleanComponent : GameComponent
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            Debug.Log("hola");
+            RaycastHit hit;
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                return;
 
-            Physics.Raycast((new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, 0.0f)), Camera.main.transform.forward, out RaycastHit hit);
-            Vector2Int localPos = new Vector2Int();
-            localPos.x = (int)(_rectTransform.position.x - hit.point.x);
-            localPos.y = (int)(_rectTransform.position.y - hit.point.y);
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+            MeshCollider meshCollider = hit.collider as MeshCollider;
 
-            int pixelX = localPos.x /  dirtMaskTexture.width;
-            int pixelY = localPos.x / dirtMaskTexture.width;
+            if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+                return;
 
-            Debug.Log(pixelX + " " + pixelY);
-            Debug.Log(dirtMaskTexture.GetPixel(pixelX, pixelY));
-            if (dirtMaskTexture.GetPixel(pixelX, pixelY) != Color.black)
-            {
-                Debug.Log("te pinto negro");
-                dirtMaskTexture.SetPixel(pixelX, pixelY, Color.black);
-            }
+            Texture2D tex = rend.material.mainTexture as Texture2D;
+            Vector2 pixelUV = hit.textureCoord;
+            pixelUV.x *= tex.width;
+            pixelUV.y *= tex.height;
+
+            tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+            tex.Apply();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Debug.Log("hola");
+
+            //Physics.Raycast((new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, 0.0f)), Camera.main.transform.forward, out RaycastHit hit);
+            //Vector2Int localPos = new Vector2Int();
+            //localPos.x = (int)(_rectTransform.position.x - hit.point.x);
+            //localPos.y = (int)(_rectTransform.position.y - hit.point.y);
+
+            //int pixelX = localPos.x /  dirtMaskTexture.width;
+            //int pixelY = localPos.x / dirtMaskTexture.width;
+
+            //Debug.Log(pixelX + " " + pixelY);
+            //Debug.Log(dirtMaskTexture.GetPixel(pixelX, pixelY));
+            //if (dirtMaskTexture.GetPixel(pixelX, pixelY) != Color.black)
+            //{
+            //    Debug.Log("te pinto negro");
+            //    dirtMaskTexture.SetPixel(pixelX, pixelY, Color.black);
+            //}
         }
     }
 }
