06ca59e2c3b976714835a14bef5f19a577f4c0a8
Rafael Argandoña Blacido <rargan01@ucm.es>
2025-01-25 20:01:44 +0100

    Miau

--- Global2025/Assets/Scripts/Input/HandCursorComponent.cs
+++ Global2025/Assets/Scripts/Input/HandCursorComponent.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HandCursorComponent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private BubbleComponent _currentBubble = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        _currentBubble = collision.GetComponent<BubbleComponent>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _currentBubble = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Input.mousePosition;
+        if (_currentBubble != null && Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            _currentBubble.Pop();
+        }
     }
 }
