c7b53a42ba69eab7024b944c93fff139667f3ea6
algonz39 <algonz39@ucm.es>
2025-01-25 17:01:17 +0100

    Ventanas con scale y otros arreglos

--- Global2025/Assets/Scripts/Desktop/WindowController.cs
+++ Global2025/Assets/Scripts/Desktop/WindowController.cs
@@ -5,34 +5,38 @@ using UnityEngine.Rendering;
 
 public class WindowController : MonoBehaviour
 {
-    private bool _isDragging;
+    private bool _isDragging, _isScaling;
+    private float _startScale;
+    private int _corner;
     private Vector3 _offset;
+    private Vector2 _scalePosition;
     private RectTransform _rectTransform;
     private WindowManager _windowManager;
     private BoxCollider2D _boxCollider;
     private Canvas _canvas;
 
     [SerializeField]
-    private RectTransform _rectTransformBar;
-
+    private RectTransform _rectTransformBar, _leftCorner, _rightCorner;
     [SerializeField]
     private MinigameManager _gameManager;
-
-    private float _barHeightHalfSize;
+    [SerializeField]
+    private float _maxScale = 1.5f, _minScale = 0.67f, _scaleFactor = 0.001f;
+    private float _barHalfHeight, _cornerHalfSize;
     private void Awake()
     {
         _windowManager = GetComponentInParent<WindowManager>();
-        if(!_gameManager) _gameManager = GetComponentInParent<MinigameManager>();
+        if(!_gameManager) _gameManager = GetComponentInChildren<MinigameManager>();
         _canvas = GetComponent<Canvas>();
         _rectTransform = GetComponent<RectTransform>();
     }
+
     private void Start()
     {
         _isDragging = false;
         _boxCollider = GetComponent<BoxCollider2D>();
-        _barHeightHalfSize = _rectTransformBar.sizeDelta.y / 2;
+        _barHalfHeight = _rectTransformBar.sizeDelta.y / 2;
+        _cornerHalfSize = _leftCorner.sizeDelta.y / 2;
     }
-
 
     private void OnEnable()
     {
@@ -50,15 +54,26 @@ public class WindowController : MonoBehaviour
                 _isDragging = true;
                 _offset = _rectTransform.position - (Vector3)Mouse.current.position.ReadValue();
             }
+            else if ((_corner = CheckMouseOnCorner()) > 0)
+            {
+                _scalePosition = Mouse.current.position.ReadValue();
+                _startScale = _rectTransform.localScale.x;
+                _isScaling = true;
+            }
             _windowManager.UpdateController(this);
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             _isDragging = false;
+            _isScaling = false;
         }
         if (_isDragging)
         {
             _rectTransform.position = (Vector3)Mouse.current.position.ReadValue() + _offset;
+        }
+        if (_isScaling)
+        {
+            Scale();
         }
     }
 
@@ -70,9 +85,29 @@ public class WindowController : MonoBehaviour
 
     private bool CheckMouseOnBar()
     {
-        return Mouse.current.position.ReadValue().y >= _rectTransformBar.position.y - _barHeightHalfSize;
+        return Mouse.current.position.ReadValue().y >= _rectTransformBar.position.y - _barHalfHeight;
     }
 
+    private int CheckMouseOnCorner() 
+    {
+        if (Mouse.current.position.ReadValue().y <= _leftCorner.position.y + _cornerHalfSize)
+        {
+            if (Mouse.current.position.ReadValue().x <= _leftCorner.position.x + _cornerHalfSize)
+                return 1;
+            else if (Mouse.current.position.ReadValue().x >= _rightCorner.position.x - _cornerHalfSize)
+                return 2;
+        }
+        return 0;
+    }
+
+    private void Scale()
+    {
+        Vector2 dir = Mouse.current.position.ReadValue() - _scalePosition;
+        if (_corner == 1) dir.x = -dir.x;
+        float targetScale = _startScale * (1 + (dir.x - dir.y) * _scaleFactor);
+        targetScale = Mathf.Clamp(targetScale, _minScale, _maxScale);
+        _rectTransform.localScale = new Vector3(targetScale, targetScale, targetScale);
+    }
 
     public void CloseWindow()
     {
